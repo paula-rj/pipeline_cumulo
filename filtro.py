6d951aff648411318b7b50262b26e32252931ea4
Paula <paula.rjure@gmail.com>
2023-03-31 10:39:25 -0300

    fft con codigo de sergio

--- filtro.py
+++ filtro.py
@@ -34,14 +34,56 @@ plt.title("Recorte dif con vmax 1")
 
 #%%%%%%%%%%%%%%%
 # Aplicamos Fourier sobre una fila
-fila = band1[100, :]
-suma = np.sum(band1, axis=1)
+#fila = band1[100, :]
+dif_img = band1[1:] - band1[:-1]
+suma = np.sum(dif_img, axis=0)
+
 print(suma.shape)
+
 dif_suma = suma[1:] - suma[:-1]
-fdif = np.fft.fft(dif_suma)
-plt.plot(fdif)
+
+fdif = np.fft.fft(suma)
+
+plt.plot(np.abs(fdif))
 plt.title("Fourier de diferencia sobre suma total")
 #%%%%%%%%%%%%%%%%
+import statistics as st
+std =np.std(abs(fdif[1:]))
+media =np.mean(abs(fdif[1:]))
+
+print(media, std)
+
+af_fil = np.zeros(len(fdif))
+af_fil[1:] = abs(fdif[1:])
+af_fil[af_fil < media+ 2*std] = 0
+af_fil[af_fil >= media+ 2*std] = 1
+
+posmax = np.argwhere(af_fil == 1)
+posmax = np.reshape(posmax, [len(posmax)])
+
+difpos = posmax[1:]- posmax[:-1]
+
+moda = st.mode(difpos)
+
+masc = np.zeros_like(difpos)
+masc[difpos == moda] = 1
+
+if masc.sum() / len(difpos) > .5:
+    print('--------------------------------------------')
+    print('--La imagen tiene ruido periodico-----------')
+    print('--------------------------------------------')
+
+print(masc.sum() , len(difpos), masc.sum() / len(difpos))
+
+
+plt.plot(af_fil)
+
+
+
+
+
+
+#%%%
 # Armamos un filtro y le pasamos
 ker = np.array([[1, 0, -1], [1, 0, -1], [1, 0, -1]])
 
@@ -49,4 +91,17 @@ filtered = scipy.signal.correlate2d(dif, ker)
 # %%
 plt.imshow(filtered[:128, :128], cmap="gray", vmin=-1, vmax=1)
 plt.title("Recorte de filtrado")
+# %%%%%%%%%%%%%%%%%%%%%%%%%%
+suma_filt = np.sum(filtered, axis=0)
+plt.plot(suma_filt)
+
+print(suma_filt.shape)
+#%%%%%
+fft_suma_filt = np.fft.fft(suma_filt)
+plt.plot(np.abs(fft_suma_filt))
+
+# %%
+af = np.fft.fft(suma)
+
+plt.plot(abs(af[1:]))
 # %%
