5cfc8b618c2ecde4b75a90b11b48d95112b8c263
paula-rj <paula.rjure@gmail.com>
2023-05-09 13:46:49 -0300

    reorganice dirs y agregue init

---
+++ __init__.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# =============================================================================
+# META
+# =============================================================================
+
+r"""Pipeline CUMULO.
+
+Download files from CUMULO (Dropbox).
+
+Module main has the whole process integrated.
+"""
+
+__name__ = "pipeline_cumulo"
+__version__ = "0.0.1"
+
+
+# =============================================================================
+# IMPORTS
+# =============================================================================
+
+from .pipeline import *  # noqa
+from .main import *  # noqa
+from .dict_gen import *  # noqa
