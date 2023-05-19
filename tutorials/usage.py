1144e8641714cb03954322cebf0edde76dde2b2e
paula-rj <paula.rjure@gmail.com>
2023-05-19 00:46:54 -0300

    added saving tests as json

--- tutorials/usage.py
+++ tutorials/usage.py
@@ -4,13 +4,15 @@ import json
 import os
 import tempfile
 import zipfile
-import sh 
+import sh
 import h5py
 import matplotlib.pyplot as plt
 import numpy as np
-#%%
+
+# %%
 import sys
-from pathlib import Path # if you haven't already done so
+from pathlib import Path  # if you haven't already done so
+
 file = Path(__file__).resolve()
 parent, root = file.parent, file.parents[1]
 sys.path.append(str(root))
@@ -21,21 +23,26 @@ import pipeline
 # %%Download file from 1 day
 PATH_FOR_HDF5 = "../data/"
 # Elije el json del mes
-path_json = "../links_dir/2008_01.json"
+path_json = "../links_dir/2016_01.json"
 # Guarda la parte de la fecha
-new_dir = path_json[-12:-5]  
+new_dir = path_json[-12:-5]
 f = open(path_json)
 links = json.load(f)
 # dias -> da lista de urls de cada dia
 days = list(links.keys())
 print(len(days))
-# lista de urls de cada dia 
-links_list = links[days[5]]
-url = links_list[-1]
 
+# %% lista de urls de cada dia
+
+di = 25
+links_list = links[days[di]]
+# %%
+k = 60
+url = links_list[k]
+print(url)
 # Extracts file name from full url
 date = url[90:104][3:].replace(".", "")
-#%%Downloading
+# %%Downloading
 # Generates temp file to store downloaded file
 _, tmp_path = tempfile.mkstemp(suffix=".nc", prefix=date, dir=None, text=False)
 atexit.register(os.remove, tmp_path)
@@ -49,7 +56,7 @@ norm_bands = pipeline.processing(tmp_path)
 
 # estos son h5 que se guarda en la memoria
 a = pipeline.generate_tiles(norm_bands, url[90:104], PATH_FOR_HDF5)
-#%%Testing
+# %%Testing
 files = os.listdir(PATH_FOR_HDF5)
 files.remove("cache")
 
@@ -59,6 +66,7 @@ for i in range(len(files)):
     test_list.append(pipeline.test_int(f"{PATH_FOR_HDF5}/{files[i]}"))
 
 print(len(test_list))
+print(test_list)
 
 arr = np.array(test_list)
 mal = np.argwhere(arr == 0)
@@ -67,16 +75,20 @@ print(len(mal))
 print(mal)
 
 f.close()
+# %%
+a_json = {"id": url[90:104][3:], "files_names": files, "test": test_list}
 
-#%% Plot
-dir_to_hdf5 = f"{PATH_FOR_HDF5}/{files[1]}"
+with open(f"{days[di]}.json", "w") as final:
+    json.dump(a_json, final)
+# %% Plot
+dir_to_hdf5 = f"{PATH_FOR_HDF5}/{files[100]}"
 f = h5py.File(dir_to_hdf5, "r")
 img = f["bands"][:]
 
 for i in range(6):
-    plt.imshow(a[:, :, i], cmap="gray")
+    plt.imshow(img[:, :, i], cmap="gray")
     plt.show()
-    
+
 # ONly one
 # plt.imshow(img[:, :, 1], cmap="gray")
 # plt.show()
